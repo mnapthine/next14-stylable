32f625cc9a767c9d2a5c1cc1689d5586822fa956
Mark Napthine <mnapthine@gmail.com>
2024-03-27 14:20:54 +0000

    Remove ref to shelley index file

--- src/components/ReactLiveBlock/ReactLiveScope.tsx
+++ src/components/ReactLiveBlock/ReactLiveScope.tsx
@@ -1,17 +1,12 @@
 import React from "react";
-import FocusLock from "react-focus-lock";
+// import FocusLock from "react-focus-lock";
 import { classes as dialog } from "@actionishope/shelley/Dialog/dialog.st.css";
-import {
-  Radio,
-  RadioGroup,
-  Button,
-  ButtonGroup,
-  Icon,
-  DialogTrigger,
-  Dialog,
-  P,
-  H2,
-} from "@actionishope/shelley";
+
+import { Radio, RadioGroup } from "@actionishope/shelley/Radio";
+import { Button, ButtonGroup } from "@actionishope/shelley/Button";
+import { Icon } from "@actionishope/shelley/Icon";
+import { DialogTrigger, Dialog } from "@actionishope/shelley/Dialog";
+import { P, H2 } from "@actionishope/shelley/Text";
 // import Lorem from 'react-lorem-component'
 // import * as Loaders from 'react-spinners'
 // import * as ReactTable from 'react-table'
