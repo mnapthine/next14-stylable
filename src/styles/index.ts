21a2a038106731bcfdedffe0e1661096c59512ca
Mark Napthine <mnapthine@gmail.com>
2024-03-28 21:29:58 +0000

    Rename text style

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
 import "./codeBlocks.st.css";
 import "./header.st.css";
-// import "./text.st.css";
+import "./textOverrides.st.css";
 import "./pageContent.st.css";
 import "./pageLayout.st.css";
 import "./tableOfContents.st.css";
