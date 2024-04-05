66a9b23e427b8f227b2d96aef57ac5296d1539e6
Mark Napthine <mnapthine@gmail.com>
2024-04-05 13:48:00 +0100

    suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={root}>
+    <html lang="en" className={root} suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="data-theme"
