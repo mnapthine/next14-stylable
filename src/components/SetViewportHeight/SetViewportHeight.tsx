1a2617eec21e1e5526ee40ded434307ac74f03b4
Mark Napthine <mnapthine@gmail.com>
2024-04-06 11:02:42 +0100

    Move to component

---
+++ src/components/SetViewportHeight/SetViewportHeight.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect } from "react";
+
+/**
+ * Sets a custom CSS variable property for the viewport height,
+ * this is useful for mobile browsers that hide the address bar
+ * on scroll. Usage:
+ * - set @property st-global(--vh); in your stylable stylesheet.
+ * - then use `var(--vh)` to get the value as usual
+ * @returns null
+ */
+export function SetViewportHeight() {
+  useEffect(() => {
+    // Update --vh custom property
+    const updateVH = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    // Update on mount and resize
+    updateVH();
+    window.addEventListener("resize", updateVH);
+
+    // Cleanup listener on component unmount
+    return () => window.removeEventListener("resize", updateVH);
+  }, []);
+
+  return null;
+}
