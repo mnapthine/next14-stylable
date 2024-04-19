db0c205ce52991576cb4a6135a0e68fd37e50119
Mark Napthine <mnapthine@gmail.com>
2024-04-19 17:23:32 +0100

    tidy

--- src/components/PageTabNav/PageTabNav.tsx
+++ src/components/PageTabNav/PageTabNav.tsx
@@ -1,9 +1,5 @@
-"use client";
 import Link from "next/link";
-import useSize from "@react-hook/size";
-// import { usePathname } from "next/navigation";
 import { st, classes } from "./pageTabNav.st.css";
-// import { useRef, useState } from "react";
 
 interface PageTabNavProps {
   className?: string;
@@ -14,14 +10,6 @@ interface PageTabNavProps {
 export function PageTabNav(props: PageTabNavProps) {
   const { className, componentUrl, activeTab } = props;
 
-  // const pathname = usePathname();
-
-  // const [activeTab, setActiveTab] = useState("usage");
-
-  // if (pathname.includes("style-api")) {
-  //   setActiveTab("styling");
-  // }
-
   return (
     <nav
       className={st(classes.root, className)}
@@ -30,14 +18,12 @@ export function PageTabNav(props: PageTabNavProps) {
       <Link
         className={st(classes.tab, { isActive: activeTab === "usage" })}
         href={componentUrl}
-        // ref={usageLink}
       >
         Usage
       </Link>
       <Link
         className={st(classes.tab, { isActive: activeTab === "styling" })}
         href={`${componentUrl}/style-api`}
-        // ref={stylingLink}
       >
         Styling
       </Link>
